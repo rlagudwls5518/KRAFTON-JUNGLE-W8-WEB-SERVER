5760488dbf7b2d49f8eb5f2b74329607d044d0dd
rlagudwls5518 <rlagudwls5518@naver.com>
2025-08-29 11:46:48 +0900

    08/29

---
+++ webproxy-lab/echo/echo_client.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "csapp.h"
+
+int open_clientfd(char *hostname, char*port);
+
+int main(int argc, char **argv) {
+    // 여기에 코드를 작성하세요
+    exit(0);
+}
+
+int open_clientfd(char *hostname, char*port) {
+    // 여기에 코드를 작성하세요
+    return -1;
+}
---
+++ webproxy-lab/echo/echo_server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "csapp.h"
+
+int open_listenfd(char *port);
+void echo(int connfd);
+
+int main(int argc, char **argv) {
+	// 여기에 코드를 작성하세요
+    exit(0);
+}
+
+int open_listenfd(char *port) {
+    // 여기에 코드를 작성하세요
+    return -1;
+}
+
+void echo(int connfd) {
+    // 여기에 코드를 작성하세요
+}
